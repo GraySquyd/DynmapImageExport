ff3c4b086522f091998a9868a206baa880132b52
Gray <octo3535@aol.com>
2024-05-23 14:06:03 -0700

    Really sloppy code for sanatizing file paths

--- DynmapImageExport/Commands/MergeCommand.cs
+++ DynmapImageExport/Commands/MergeCommand.cs
@@ -11,112 +11,125 @@ using Padding = DynmapImageExport.Models.Padding;
 
 namespace DynmapImageExport.Commands
 {
-    internal class MergeCommand : Command
-    {
-        private static readonly ProgressColumn[] Columns;
-        private static readonly Stopwatch SW = new();
+	internal class MergeCommand : Command
+	{
+		private static readonly ProgressColumn[] Columns;
+		private static readonly Stopwatch SW = new();
 
-        static MergeCommand()
-        {
-            Columns = new ProgressColumn[]    {
-                new TaskDescriptionColumn(),
-                new ProgressBarColumn(),
-                new PercentageColumn(),
-                new RemainingTimeColumn()
-            };
-        }
+		static MergeCommand()
+		{
+			Columns = new ProgressColumn[]
+			{
+				new TaskDescriptionColumn(),
+				new ProgressBarColumn(),
+				new PercentageColumn(),
+				new RemainingTimeColumn()
+			};
+		}
 
-        public MergeCommand() : base("merge", "Merge dynmap to image")
-        {
-            AddAlias("m");
-            AddArgument(new Argument<Uri>("url", "Dynmap URL"));
-            AddArgument(new Argument<string>("world", "World name"));
-            AddArgument(new Argument<string>("map", "Map name"));
-            AddArgument(new PointArgument());
-            AddOption(new PaddingOption());
-            AddOption(new ZoomOption());
-            AddOption(new OutputOption());
-            AddOption(new FormatOption());
-            AddOption(new NoCacheOption());
+		public MergeCommand() : base("merge", "Merge dynmap to image")
+		{
+			AddAlias("m");
+			AddArgument(new Argument<Uri>("url", "Dynmap URL"));
+			AddArgument(new Argument<string>("world", "World name"));
+			AddArgument(new Argument<string>("map", "Map name"));
+			AddArgument(new PointArgument());
+			AddOption(new PaddingOption());
+			AddOption(new ZoomOption());
+			AddOption(new OutputOption());
+			AddOption(new FormatOption());
+			AddOption(new NoCacheOption());
 
-            Handler = CommandHandler.Create(HandleCommand);
-        }
+			Handler = CommandHandler.Create(HandleCommand);
+		}
 
-        private static async Task<int> HandleCommand(
-            Uri URL,
-            string world,
-            string map,
-            Point[] point,
-            Padding padding,
-            int? zoom,
-            string output,
-            ImageFormat? format,
-            bool noCache)
-        {
-            AnsiConsole.MarkupLine($"[yellow]Merging of: {URL.Host} - {world} - {map}[/]");
-            using var Dynmap = await Common.GetDynmap(URL);
-            var World = Dynmap.GetWorld(world);
-            var Map = World.GetMap(map);
-            var Source = new TileSource(Dynmap, World, Map);
-            Source.ValidateZoom(zoom);
-            var Zoom = zoom ?? Source.ScaleToZoom(1);
-            var Format = format ?? Source.ImageFormat;
+		private static async Task<int> HandleCommand(
+			Uri URL,
+			string world,
+			string map,
+			Point[] point,
+			Padding padding,
+			int? zoom,
+			string output,
+			ImageFormat? format,
+			bool noCache)
+		{
+			AnsiConsole.MarkupLine($"[yellow]Merging of: {URL.Host} - {world} - {map}[/]");
+			using var Dynmap = await Common.GetDynmap(URL);
+			var World = Dynmap.GetWorld(world);
+			var Map = World.GetMap(map);
+			var Source = new TileSource(Dynmap, World, Map);
+			Source.ValidateZoom(zoom);
+			var Zoom = zoom ?? Source.ScaleToZoom(1);
+			var Format = format ?? Source.ImageFormat;
 
-            var PointTiles = point.Select(P => Source.PointToTile(P, Zoom)).ToList();
-            var Tiles = TileMap.CreateTileMap(PointTiles, padding);
+			var PointTiles = point.Select(P => Source.PointToTile(P, Zoom)).ToList();
+			var Tiles = TileMap.CreateTileMap(PointTiles, padding);
 
-            var Points = string.Join("~", point.Select(P => $"{P}"));
-            var Size = $"~{Tiles.Height} X ~{Tiles.Width}(~{Tiles.Height * 128}px X ~{Tiles.Width * 128}px)";
-            var Info = new Grid()
-                .AddColumns(2)
-                .AddRow("Points:", Points.EscapeMarkup())
-                .AddRow("Padding:", $"{padding}".EscapeMarkup())
-                .AddRow("Tiles count:", $"~{Tiles.Count}")
-                .AddRow("Image size:", Size);
-            AnsiConsole.Write(Info);
-            Trace.WriteLine($"Input: {world}-{map}-{Points}-{padding}-{Zoom}");
+			var Points = string.Join("~", point.Select(P => $"{P}"));
+			var Size = $"~{Tiles.Height} X ~{Tiles.Width}(~{Tiles.Height * 128}px X ~{Tiles.Width * 128}px)";
+			var Info = new Grid()
+				.AddColumns(2)
+				.AddRow("Points:", Points.EscapeMarkup())
+				.AddRow("Padding:", $"{padding}".EscapeMarkup())
+				.AddRow("Tiles count:", $"~{Tiles.Count}")
+				.AddRow("Image size:", Size);
+			AnsiConsole.Write(Info);
+			Trace.WriteLine($"Input: {world}-{map}-{Points}-{padding}-{Zoom}");
 
-            var FilePath = output ?? $"{Source.Title} ({world}-{map}-{Points}-{padding}-{Zoom})";
-            FilePath = Regex.Replace(FilePath, @"\.\w{3,4}$", "", RegexOptions.IgnoreCase);
+			var FilePath = output ?? $"{Source.Title} ({world}-{map}-{Points}-{padding}-{Zoom})";
+			FilePath = Regex.Replace(FilePath, @"\.\w{3,4}$", "", RegexOptions.IgnoreCase);
 
-            SW.Restart();
+			//sanatizing
+			FilePath = SanitizeFileName(FilePath);
 
-            var (Images, Merger) = await AnsiConsole.Progress()
-                 .Columns(Columns)
-                 .StartAsync(async ctx =>
-                 {
-                     var T = ctx.AddTask($"Downloading tiles: 0/{Tiles.Count}", true, Tiles.Count);
-                     var T2 = ctx.AddTask($"Merging tiles: 0/0", false);
-                     T2.IsIndeterminate = true;
+			SW.Restart();
 
-                     using var TD = new TileDownloader(Tiles, 8) { UseCache = !noCache };
-                     var Images = await TD.Download(T.AsProgress());
+			var (Images, Merger) = await AnsiConsole.Progress()
+				 .Columns(Columns)
+				 .StartAsync(async ctx =>
+				 {
+					 var T = ctx.AddTask($"Downloading tiles: 0/{Tiles.Count}", true, Tiles.Count);
+					 var T2 = ctx.AddTask($"Merging tiles: 0/0", false);
+					 T2.IsIndeterminate = true;
 
-                     T2.MaxValue = Images.Count;
-                     T2.IsIndeterminate = false;
-                     T2.StartTask();
-                     var TM = new TileMerger(Images);
-                     TM.Merge(T2.AsProgress());
-                     return (Images, TM);
-                 });
+					 using var TD = new TileDownloader(Tiles, 8) { UseCache = !noCache };
+					 var Images = await TD.Download(T.AsProgress());
 
-            var Image = AnsiConsole.Status()
-                .Spinner(Spinner.Known.Dots)
-                .Start("Saving image", ctx => Merger.Save(FilePath, Format));
-            Merger.Dispose();
+					 T2.MaxValue = Images.Count;
+					 T2.IsIndeterminate = false;
+					 T2.StartTask();
+					 var TM = new TileMerger(Images);
+					 TM.Merge(T2.AsProgress());
+					 return (Images, TM);
+				 });
 
-            SW.Stop();
-            Size = $"{Images.Height} X {Images.Width}({Images.Height * 128}px X {Images.Width * 128}px)";
-            var TP = new TextPath(Image.FullName)
-                .LeafColor(Color.Yellow);
-            var Out = new Grid()
-                .AddColumns(2)
-                .AddRow("[white]Merge done:[/]", $@"[yellow]{SW.Elapsed:hh\:mm\:ss}[/]")
-                .AddRow("[white]Tiles count:[/]", $"{Images.Count}")
-                .AddRow("[white]Image size:[/]", Size)
-                .AddRow(new Markup("[white]Image path:[/]"), TP);
-            AnsiConsole.Write(Out);
-            return 0;
-        }
-    }
+			var Image = AnsiConsole.Status()
+				.Spinner(Spinner.Known.Dots)
+				.Start("Saving image", ctx => Merger.Save(FilePath, Format));
+			Merger.Dispose();
+
+			SW.Stop();
+			Size = $"{Images.Height} X {Images.Width}({Images.Height * 128}px X {Images.Width * 128}px)";
+			var TP = new TextPath(Image.FullName)
+				.LeafColor(Color.Yellow);
+			var Out = new Grid()
+				.AddColumns(2)
+				.AddRow("[white]Merge done:[/]", $@"[yellow]{SW.Elapsed:hh\:mm\:ss}[/]")
+				.AddRow("[white]Tiles count:[/]", $"{Images.Count}")
+				.AddRow("[white]Image size:[/]", Size)
+				.AddRow(new Markup("[white]Image path:[/]"), TP);
+			AnsiConsole.Write(Out);
+			return 0;
+		}
+
+		private static string SanitizeFileName(string fileName)
+		{
+			//sanitizing with regex, aka sorcerey
+			var sanitized = Regex.Replace(fileName, @"[<>:""/\\|?*\x00-\x1F§]", "");
+
+			//trim whitespaces
+			return sanitized.Trim().Trim('.');
+		}
+	}
 }
--- DynmapImageExport/TileDownloader.cs
+++ DynmapImageExport/TileDownloader.cs
@@ -1,91 +1,99 @@
 ﻿using DynmapImageExport.Models;
 using System.Diagnostics;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace DynmapImageExport
 {
-    internal class TileDownloader : IDisposable
-    {
-        private readonly HttpClient Client = new();
-        private readonly ImageMap Files = new();
-        private readonly SemaphoreSlim Semaphore;
-        private readonly TileMap Tiles;
-        private readonly string Title;
+	internal class TileDownloader : IDisposable
+	{
+		private readonly HttpClient Client = new();
+		private readonly ImageMap Files = new();
+		private readonly SemaphoreSlim Semaphore;
+		private readonly TileMap Tiles;
+		private readonly string Title;
 
-        public TileDownloader(TileMap tiles) : this(tiles, 4) { }
+		public TileDownloader(TileMap tiles) : this(tiles, 4) { }
 
-        public TileDownloader(TileMap tiles, int threads)
-        {
-            Tiles = tiles;
-            Title = Tiles.Source.Title;
-            Semaphore = new(threads);
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-        }
+		public TileDownloader(TileMap tiles, int threads)
+		{
+			Tiles = tiles;
+			Title = SanitizeFileName(Tiles.Source.Title);
+			Semaphore = new(threads);
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+		}
 
-        public bool UseCache { get; set; } = true;
+		public bool UseCache { get; set; } = true;
 
-        public async Task<ImageMap> Download(IProgress<int> IP = default)
-        {
-            Trace.WriteLine($"Download started: {Tiles.Count} tiles");
-            // TilesURI can have URLSearchParams so BaseAddress doesn't work
+		public async Task<ImageMap> Download(IProgress<int> IP = default)
+		{
+			Trace.WriteLine($"Download started: {Tiles.Count} tiles");
 
-            Files.Clear();
-            var Tasks = Tiles.Select(async (KV) =>
-            {
-                var (DXY, Tile) = KV;
-                if (await TryDownloadTile(Tile) is string path) { Files[DXY] = path; }
-                IP.Report(1);
-            });
-            await Task.WhenAll(Tasks);
+			Files.Clear();
+			var Tasks = Tiles.Select(async (KV) =>
+			{
+				var (DXY, Tile) = KV;
+				var sanitizedTilePath = SanitizeFileName(Tile.TilePath);
+				if (await TryDownloadTile(Tile, sanitizedTilePath) is string path) { Files[DXY] = path; }
+				IP.Report(1);
+			});
+			await Task.WhenAll(Tasks);
 
-            Trace.WriteLine($"Download done: {Files.Count} tiles");
-            return Files;
-        }
+			Trace.WriteLine($"Download done: {Files.Count} tiles");
+			return Files;
+		}
 
-        private async Task<string> TryDownloadTile(Tile tile)
-        {
-            var LocalFile = new FileInfo(Path.Combine("tiles", Title, tile.TilePath));
-            if (UseCache && LocalFile.Exists)
-            {
-                Trace.WriteLine($"Cached tile: {tile.TilePath} ");
-                return LocalFile.FullName;
-            }
-            try
-            {
-                await Semaphore.WaitAsync();
-                Trace.WriteLine($"Downloading tile: {tile.TileURI} ");
-                using var Responce = await Client.GetAsync(tile.TileURI, HttpCompletionOption.ResponseHeadersRead);
-                if (!Responce.IsSuccessStatusCode)
-                {
-                    Trace.WriteLine($"Tile: {tile} - {Responce.StatusCode}({Responce.ReasonPhrase})");
-                    return null;
-                }
+		private async Task<string> TryDownloadTile(Tile tile, string sanitizedTilePath)
+		{
+			var LocalFile = new FileInfo(Path.Combine("tiles", Title, sanitizedTilePath));
+			if (UseCache && LocalFile.Exists)
+			{
+				Trace.WriteLine($"Cached tile: {sanitizedTilePath} ");
+				return LocalFile.FullName;
+			}
+			try
+			{
+				await Semaphore.WaitAsync();
+				Trace.WriteLine($"Downloading tile: {tile.TileURI} ");
+				using var Response = await Client.GetAsync(tile.TileURI, HttpCompletionOption.ResponseHeadersRead);
+				if (!Response.IsSuccessStatusCode)
+				{
+					Trace.WriteLine($"Tile: {tile} - {Response.StatusCode}({Response.ReasonPhrase})");
+					return null;
+				}
 
-                Directory.CreateDirectory(LocalFile.DirectoryName);
-                using var FS = new FileStream(LocalFile.FullName, FileMode.Create);
-                using var RS = await Responce.Content.ReadAsStreamAsync();
-                await RS.CopyToAsync(FS);
+				Directory.CreateDirectory(LocalFile.DirectoryName);
+				using var FS = new FileStream(LocalFile.FullName, FileMode.Create);
+				using var RS = await Response.Content.ReadAsStreamAsync();
+				await RS.CopyToAsync(FS);
 
-                return LocalFile.FullName;
-            }
-            catch (HttpRequestException e)
-            {
-                Trace.WriteLine($"Tile: {tile} - {e.Message}");
-            }
-            finally
-            {
-                Semaphore.Release();
-            }
-            return null;
-        }
+				return LocalFile.FullName;
+			}
+			catch (HttpRequestException e)
+			{
+				Trace.WriteLine($"Tile: {tile} - {e.Message}");
+			}
+			finally
+			{
+				Semaphore.Release();
+			}
+			return null;
+		}
 
-        #region IDispose
+		private static string SanitizeFileName(string fileName)
+		{
+			// more witchcraft, sanatizing w/ regex
+			var sanitized = Regex.Replace(fileName, @"[<>:""/\\|?*\x00-\x1F§]", "");
+			return sanitized.Trim().Trim('.');
+		}
 
-        public void Dispose()
-        {
-            ((IDisposable)Client).Dispose();
-        }
+		#region IDispose
 
-        #endregion IDispose
-    }
+		public void Dispose()
+		{
+			((IDisposable)Client).Dispose();
+		}
+
+		#endregion IDispose
+	}
 }
